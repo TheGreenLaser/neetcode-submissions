ae3355087ace50115aa9f261676b92c73c085c88
Jason Wang <63211303+TheGreenLaser@users.noreply.github.com>
2026-09-10 14:48:08 -0400

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.py
@@ -0,0 +1,32 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        answer = 0
+        freq_table = dict()
+
+        l = 0
+        r = 0
+        max_freq = 0
+
+        while r < len(s):
+            r += 1
+            cur_char = s[r - 1]
+            if cur_char in freq_table:
+                freq_table.update({cur_char : freq_table.get(cur_char) + 1})
+            else:
+                freq_table.update({cur_char : 1})
+
+            max_freq = max(max_freq, freq_table.get(cur_char))
+            need_change = (r - l) - max_freq
+
+            while need_change > k:
+                l += 1
+
+                left_char = s[l - 1]
+                freq_table.update({left_char : freq_table.get(left_char) - 1})
+                max_freq = max(max_freq, freq_table.get(cur_char))
+                need_change = (r - l) - max_freq
+
+            answer = max(r - l, answer)
+        
+        return answer
+
